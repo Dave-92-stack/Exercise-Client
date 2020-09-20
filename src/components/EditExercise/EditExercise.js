28c96c2ac117318cf347489dbee50a67a5db4272
Dave-92-stack <dave.sher92@gmail.com>
2020-09-20 16:23:42 -0400

    Attempted to add the ability to edit an exercise log

---
+++ src/components/EditExercise/EditExercise.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import axios from 'axios'
+import apiUrl from '../../apiConfig'
+import messages from '../AutoDismissAlert/messages'
+import ExerciseForm from '../EditForm/EditForm'
+
+class EditExercise extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      exercise: {
+        date: '',
+        benchPressWeight: '',
+        benchPressReps: '',
+        backSquatWeight: '',
+        backSquatReps: ''
+      },
+      updated: null,
+      user: props.user
+    }
+  }
+  componentDidMount () {
+    axios({
+      url: `${apiUrl}/exercises/${this.props.match.params.id}`,
+      method: 'GET',
+      headers: {
+        'Authorization': `Token token=${this.state.user.token}`
+      }
+    })
+      .then(res => this.setState({ exercise: res.data.exercise }))
+      .catch(console.error)
+  }
+  handleChange = event => {
+    event.persist()
+    this.setState(prevState => {
+      const updatedField = { [event.target.name]: event.target.value }
+      const editedExercise = Object.assign({}, prevState.exercise, updatedField)
+      return { exercise: editedExercise }
+    })
+  }
+  handleSubmit = event => {
+    event.preventDefault()
+    const { msgAlert } = this.props
+    axios({
+      url: `${apiUrl}/exercises/${this.props.match.params.id}`,
+      method: 'PATCH',
+      data: { exercise: this.state.exercise },
+      headers: {
+        'Authorization': `Token token=${this.state.user.token}`
+      }
+    })
+      .then(() => msgAlert({
+        heading: 'Exercise log updated!',
+        message: messages.editExerciseSuccess,
+        variant: 'success'
+      }))
+      .then(res => this.setState({ updated: true }))
+
+      .catch(() =>
+        msgAlert({
+          heading: 'Exercise failed to update!',
+          message: messages.editExerciseFailure,
+          variant: 'danger'
+        })
+      )
+  }
+
+  handleDelete = () => {
+    const { msgAlert } = this.props
+
+    axios({
+      url: `${apiUrl}/exercises/${this.props.match.params.id}`,
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Token token=${this.state.user.token}`
+      }
+    })
+      .then(() => this.setState({ updated: true }))
+      .then(() => msgAlert({
+        heading: 'Exercise log deleted successfully!',
+        message: messages.deleteExerciseSuccess,
+        variant: 'success'
+      }))
+  }
+  render () {
+    const { exercise, updated } = this.state
+    const { handleChange, handleSubmit, handleDelete } = this
+    if (updated) {
+      return <Redirect to='/my-exercises' />
+    }
+    return (
+      <ExerciseForm
+        exercise={exercise}
+        handleDelete={handleDelete}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        cancelPath='/exercises'
+      />
+    )
+  }
+}
+export default EditExercise
---
+++ src/components/EditForm/EditForm.js
@@ -0,0 +1,60 @@
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+const EditForm = ({ exercise, handleSubmit, handleChange, handleDelete }) => {
+  return (
+    <div className="row2">
+      <div className="col-sm-10 col-md-8 mx-auto mt-5">
+        <h3>Log Workout</h3>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>Date</Form.Label>
+            <Form.Control
+              placeholder='Date'
+              value={exercise.date}
+              name='date'
+              type='date'
+              onChange={handleChange}
+            />
+            <Form.Label>Bench Press Weight</Form.Label>
+            <Form.Control
+              placeholder='Record the weight you used.'
+              value={exercise.benchPressWeight}
+              name='benchPressWeight'
+              onChange={handleChange}
+            />
+            <Form.Label>Bench Press Reps</Form.Label>
+            <Form.Control
+              placeholder='Record your repetitions.'
+              value={exercise.benchPressReps}
+              name='benchPressReps'
+              onChange={handleChange}
+            />
+            <Form.Label>Back Squat Weight</Form.Label>
+            <Form.Control
+              placeholder='Record the weight you used.'
+              value={exercise.backSquatWeight}
+              name='backSquatWeight'
+              onChange={handleChange}
+            />
+            <Form.Label>Back Squat Reps</Form.Label>
+            <Form.Control
+              placeholder='Record your repetitions.'
+              value={exercise.backSquatReps}
+              name='backSquatReps'
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Button varient="primary" type="submit">Submit</Button>
+          <Link to="/my-exercises">
+            <Button variant="secondary">Cancel</Button>
+          </Link>
+        </Form>
+      </div>
+    </div>
+  )
+}
+export default EditForm
