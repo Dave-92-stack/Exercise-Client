fb876bcd665ea357225f38eded7bda753f24b6c0
Dave-92-stack <dave.sher92@gmail.com>
2020-09-22 12:14:58 -0400

    More deployment troubleshooting

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://whispering-retreat-24051.herokuapp.com/',
+  production: 'https://whispering-retreat-24051.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
